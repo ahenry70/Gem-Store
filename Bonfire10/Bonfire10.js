03ef22e1a0a8f9135245589fd02b819c27ed7344
Abel Henry <abel.henry@cinemassivedisplays.local>
2016-04-15 16:39:51 -0400

    completed

--- Bonfire10/Bonfire10.js
+++ Bonfire10/Bonfire10.js
@@ -1,13 +1,22 @@
-function reverseString(str) {
+function sumAll(arr) {
 
-var result = "";
-var index = str.length-1;
-while (index >= 0){
-  result += str[index];
-  index--;
+
+if (arr[0] == arr[1]){
+  return arr[0] + arr[1];
+  }else{
+var  largerNum = Math.max(arr[0], arr[1]);
+var  smallerNum = Math.min(arr[0], arr[1]);
+}
+
+var result=smallerNum;
+var diff = largerNum-smallerNum;
+var i=1;
+
+while (i < diff+1){
+  result += smallerNum + i;
+  i++;
 }
 
 return result;
-}
 
-reverseString("hello");
+}
--- Bonfire10/test/test.js
+++ Bonfire10/test/test.js
@@ -1,16 +1,20 @@
-describe('reverseString', function(){
-  it('Should return a when a is entered', function(){
-    expect(reverseString("a")).toEqual("a");}
+describe('sumAll', function(){
+  it('Entered ([1,1]) -- Expect (2)', function(){
+    expect(sumAll([1,1])).toEqual(2);}
   );
-  it('Should return b when b is entered', function(){
-    expect(reverseString("b")).toEqual("b");}
+  it('Entered ([1,2]) -- Expect (3)', function(){
+    expect(sumAll([1,2])).toEqual(3);}
   );
-
-  it('Should return ba when ab is entered', function(){
-    expect(reverseString("ba")).toEqual("ab");}
+  it('Entered ([1,3]) -- Expect (6)', function(){
+    expect(sumAll([1,3])).toEqual(6);}
   );
-
-  it('Should return bca when abc is entered', function(){
-    expect(reverseString("cba")).toEqual("abc");}
+  it('Entered ([10,5]) -- Expect (45)', function(){
+    expect(sumAll([10,5])).toEqual(45);}
+  );
+  it('Entered ([1,4]) -- Expect (10)', function(){
+    expect(sumAll([1,4])).toEqual(10);}
+  );
+  it('Entered ([-1,4]) -- Expect (9)', function(){
+    expect(sumAll([-1,4])).toEqual(9);}
   );
 });
